a7298986197a52481dedff3346b0ff48af6d3c2c
rtoal <rtoal@lmu.edu>
2012-06-02 22:41:06 -0700

    Progress

--- src/main/java/edu/lmu/cs/xlg/rtoal/exercises/joda/Exercises.java
+++ src/main/java/edu/lmu/cs/xlg/rtoal/exercises/joda/Exercises.java
@@ -1,19 +1,23 @@
 package edu.lmu.cs.xlg.rtoal.exercises.joda;
 
+import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
 
 /**
  * A utility class containing exercises for you to fill in.
  *
- * <p>Fill in all the method stubs according to the "instructions" in the javadoc comments.
+ * <p>Fill in all the method stubs according to the "instructions" in the methods' comments.
  * For more help on what the methods are supposed to do, see the unit test file.  (You already
  * knew that.)</p>
+ *
+ * <p>We gave you the first one, daysUntilNewYear.  It was taken from the Joda-Time main page
+ * on SourceForge.</p>
  */
 public class Exercises {
 
     /**
-     * Returns the number of days from the given datetime until the (next) New Years Day.
+     * Returns the number of days from the given local date until the (next) New Years Day.
      */
     public static Days daysUntilNewYear(LocalDate date) {
         LocalDate newYear = date.plusYears(1).withDayOfYear(1);
@@ -21,9 +25,26 @@ public class Exercises {
     }
 
     /**
-     * A method to print a message in case someone thinks this is an application class.
+     * Returns the ISO8601 of the datetime string, in New York, at epoch time 1000000000000.
      */
-    public static void main(String[] args) {
-        System.out.println("Nothing to run here.  Get your unit tests to pass.");
+    public static String epochTimeOneTrillionInNewYork() {
+        return null;
     }
+
+    /**
+     * Returns the datetime one billion seconds after a given datetime.
+     */
+    public static DateTime oneBillionSecondsAfter(DateTime dateTime) {
+        return null;
+    }
+
+    /**
+     * Return the name of the day of the week, in French, of Bastille Day (July 14) in the year
+     * 2020.
+     */
+    public static String frenchDayNameOfBastilleDay2020() {
+        return null;
+    }
+
+    // TODO - SEVEN MORE STUBS AT LEAST
 }
--- src/test/java/edu/lmu/cs/xlg/rtoal/exercises/joda/ExerciseTest.java
+++ src/test/java/edu/lmu/cs/xlg/rtoal/exercises/joda/ExerciseTest.java
@@ -1,11 +1,12 @@
 package edu.lmu.cs.xlg.rtoal.exercises.joda;
 
-import org.junit.Test;
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
+import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
+import org.junit.Test;
 
 /**
  * The unit tests for the Joda-Time exercises.
@@ -18,5 +19,20 @@ public class ExerciseTest {
         assertThat(Exercises.daysUntilNewYear(new LocalDate(2003, 12, 31)), is(Days.ONE));
         assertThat(Exercises.daysUntilNewYear(new LocalDate(2003, 1, 1)), is(Days.days(365)));
         assertThat(Exercises.daysUntilNewYear(new LocalDate(2004, 1, 1)), is(Days.days(366)));
+    }
+
+    @Test
+    public void testNewYorkTrillion() {
+        assertThat(Exercises.epochTimeOneTrillionInNewYork(), is("2001-09-08T21:46:40.000-04:00"));
+    }
+
+    @Test
+    public void testBastilleDay2020() {
+        assertThat(Exercises.frenchDayNameOfBastilleDay2020(), is("mardi"));
+    }
+
+    @Test
+    public void testOneBillionSeconds() {
+        assertThat(Exercises.oneBillionSecondsAfter(new DateTime(5)), is(new DateTime(1000000000005L)));
     }
 }
